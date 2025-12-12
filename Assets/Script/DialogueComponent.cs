f28c84bf37847a00dfd95acd6f9624797378af6e
Kekelios <cvlgdcours.projets@gmail.com>
2025-12-12 16:55:44 +0100

    Push cours 12 12

---
+++ Assets/Script/DialogueComponent.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class DialogueComponent : MonoBehaviour, IActionnable
+{
+    [SerializeField] private DialogueDatas _dialogueData;
+    private DialogueRow _currentRow;
+    private int _currentRowIndex = 0;
+    [SerializeField] private UIDialogueController _dialogueController;
+
+    public void Action(Pawn CurrentPawn)
+    {
+        _currentRow = GetDialogueRow();
+        // afficher ma ligne 
+        _dialogueController.StartDialogue(this);
+    }
+
+    public DialogueRow GetDialogueRow()
+    {
+        return _dialogueData.rows[_currentRowIndex];
+    }
+
+    public string GetDialogueText()
+    {
+        return _currentRow.longDialogue;
+    }
+
+    public string GetCharacterName()
+    {
+        return _currentRow.characterName;
+    }
+
+    public void GetNextRow()
+    {
+        if (_currentRow.nextRowNumber == -1)
+        {
+            _dialogueController.EndDialogue(); 
+        }
+        else
+        {
+            _currentRowIndex = _currentRow.nextRowNumber;
+            _currentRow = GetDialogueRow();
+            _dialogueController.UpdateText();
+        }
+    }
+}   
---
+++ Assets/Script/DialogueData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[System.Serializable]
+public struct DialogueRow
+{
+    public int rowNumber; // pas vraiment utile 
+    public string characterName;
+    public string longDialogue;
+    public int nextRowNumber;
+}
+
+[CreateAssetMenu(fileName = "DialogueDatas", menuName = "Scriptable Objects/DialoguesDatas")]
+
+public class DialogueDatas : ScriptableObject
+{
+    public DialogueRow[] rows;
+}
